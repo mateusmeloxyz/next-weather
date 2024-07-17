3d2e1905bb1c9680c74e67848f51915e51b48ba3
Mateus Melo <melo.mateus@proton.me>
2024-07-17 07:16:36 -0300

    The Complete Developer ch 5 nextjs

---
+++ utils/fetch-names.ts
@@ -0,0 +1,15 @@
+export const fetchNames = async () => {
+    const url = "https://www.usemodernfullstack.dev/api/v1/now";
+    let data: responseItemType[] | [] = [];
+    let names: responseItemType[] | [];
+    try {
+        const response = await fetch(url);
+        data = (await response.json()) as responseItemType[];
+    } catch (err) {
+        names = [];
+    }
+    names = data.map((item) => {
+        return { id: item.id, name: item.name };
+    });
+    return names;
+};
