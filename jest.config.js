ce9495acb13c040f53353bcb516009a09650c074
Mateus Melo <melo.mateus@proton.me>
2024-08-10 18:31:30 -0300

    Exercise 8 from testing chapter

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({});
+
+module.exports = createJestConfig(nextJest({}));
